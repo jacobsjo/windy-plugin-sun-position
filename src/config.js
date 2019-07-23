a27a7a124fe54afc1557c386d78880c6874fdb6a
Jochen Jacobs <jochen.jacobs@campus.tu-berlin.de>
2019-07-23 17:13:42 +0200

    fixed plugin load and unload bugs

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 module.exports = {
 	displayName: 'Sun Position',
 
-	hook: 'contextmenu',
+	hook: 'menu',
 
 	dependencies: [
 		'https://unpkg.com/d3@5.7.0/dist/d3.min.js',
-		'https://unpkg.com/suncalc@1.8.0/suncalc.js',
-		'https://unpkg.com/leaflet-geometryutil@0.9.1/src/leaflet.geometryutil.js'
+		'https://unpkg.com/suncalc@1.8.0/suncalc.js'
 	],
+
+//	className: 'plugin-lhpane',
 
 	className: 'plugin-lhpane plugin-mobile-fullscreen',
 
